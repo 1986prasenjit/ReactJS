6b2c65654834dd6d3cc65559decb557c4be98a3c
1986prasenjit <prasenjitbasu44@gmail.com>
2025-09-22 00:54:19 +0530

    Fixed Bugged-1 for component re-rendering used React.memo

---
+++ Day-8-BugFix/src/App.jsx
@@ -0,0 +1,13 @@
+import { ParentComponent } from "./components/Problem-1";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ParentComponent />
+      </div>
+    </>
+  );
+}
+
+export default App;

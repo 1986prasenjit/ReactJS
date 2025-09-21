3511308f15dbf66d91b9b7c46c0c949841018303
1986prasenjit <prasenjitbasu44@gmail.com>
2025-09-22 01:15:54 +0530

    Added BugFix-2, fixed the bug for the counter

--- Day-8-BugFix/src/App.jsx
+++ Day-8-BugFix/src/App.jsx
@@ -1,10 +1,12 @@
 import { ParentComponent } from "./components/Problem-1";
+import Counter from "./components/Problem-2";
 
 function App() {
   return (
     <>
       <div>
         <ParentComponent />
+        <Counter />
       </div>
     </>
   );

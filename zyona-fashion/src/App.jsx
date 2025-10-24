2344cb16128281971a640198715adb18e6a452fa
Prasenjit1986 <prasenjitbasu44@gmail.com>
2025-10-25 03:11:54 +0530

    features updated added required folders and structured it for now and only styles the Header little bit left

--- zyona-fashion/src/App.jsx
+++ zyona-fashion/src/App.jsx
@@ -1,9 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import {
+  About,
+  Cart,
+  Collections,
+  Contact,
+  Home,
+  Login,
+  Orders,
+  PlaceOrder,
+  Product,
+} from "./pages";
+import Layout from "./Layout";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold uppercase">Zyona</h1>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
   );
 }
